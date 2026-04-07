5a068d7b3733a752148c694d6e35a8f36761a379
Real Mohsin <real@realmohsin.com>
2026-04-07 18:22:12 -0400

    Add: concatenation-of-array - submission-0

---
+++ Data Structures & Algorithms/concatenation-of-array/submission-0.js
@@ -0,0 +1,14 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[]}
+     */
+    getConcatenation(nums) {
+        const ans = new Array(2 * nums.length)
+        for (let i = 0; i < nums.length; i++) {
+            ans[i] = nums[i]
+            ans[i + nums.length] = nums[i]
+        }
+        return ans
+    }
+}
